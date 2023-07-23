3ad8287cfc92ba0198d9dec1881f745080e07feb
Penny Lin <ox1566@gmail.com>
2023-07-23 21:38:36 +0800

    'initialization'

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    // 添加 VueLoaderPlugin 插件
+    new VueLoaderPlugin(),
+  ],
+};
